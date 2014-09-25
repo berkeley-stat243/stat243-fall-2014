570fb6f8561c92a7154a37983de7aa6099fbfd1b
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2014-09-24 17:08:40 -0700

    updated testRnw files in literate programming

--- literateProgramming/testRnw.R
+++ literateProgramming/testRnw.R
@@ -3,3 +3,14 @@
 d <- 9
 e <- 11
 f <- 12
+
+## @knitr fake chunk
+
+# some code that I don't want in the chunks incorporated into my document
+
+blah = 'asdf'
+blah2q <- 'dsfsdf'
+
+## @knitr chunk6
+
+g <- 13
