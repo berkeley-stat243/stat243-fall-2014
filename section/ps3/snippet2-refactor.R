0230311785107ee65f2002282941e1c26163de09
Jarrod Millman <jarrod.millman@gmail.com>
2014-10-20 11:51:06 -0700

    Small improvements

--- section/ps3/snippet2-refactor.R
+++ section/ps3/snippet2-refactor.R
@@ -1,4 +1,6 @@
-speeches <- list("We begin.", "God bless us!")
+library(stringr)
+
+speeches <- list("We begin as a republic, America.", "God bless us!")
 
 patterns <- c("I", "we|We", "America", "democra", "republic", "Democrat",
              "Republican", "free", "war", "God[^ bless]",
@@ -6,6 +8,17 @@ patterns <- c("I", "we|We", "America", "democra", "republic", "Democrat",
              "Jesus|Christ|Christian", "job/Job/jobs")
 
 
+# will this?
+counts <- t(sapply(speeches, function(speech) {
+                                 sapply(patterns, str_count, speech)}))
+colnames(counts) <- patterns
+
+# will this?
+counts <- t(sapply(speeches, function(speech) {
+                                 sapply(patterns, str_count, string=speech)}))
+colnames(counts) <- patterns
+
+# will this work?
 counts <- t(sapply(speeches, str_count, patterns))
 colnames(counts) <- patterns
 
